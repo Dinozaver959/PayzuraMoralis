478c5c208e9604f8aa0a61842c3fbf43761841d3
Vijay Bhatiya <vijaybhatiya007@gmail.com>
2022-08-07 00:37:14 +0530

    this is currently on-going task for header popup

--- components/AccountModel.js
+++ components/AccountModel.js
@@ -1,32 +1,54 @@
 import Link from "next/link";
+import dynamic from "next/dynamic";
 import Button from "./ui/Button";
 
+import LinkExplorerIc from "./icons/Explorer";
+import CircleCheckIc from "./icons/CircleCheck";
+
 function AccountModel(props) {
+  // const textToCopy = props.currentAccount;
+  // const CC = dynamic(
+  //   () =>
+  //     import("./icons/copy-clipboard").then((mod) => mod.CopyClipboard),
+  //   { ssr: false }
+  // );
+
   return (
-    <div className='connectedAccount'>
-      <div className='accountBlock'>
-        <div className='blockTop'>
-          <div className='connectedWith'>Connected with MetaMask</div>
-          <div className='connectedActions'>
+    <div className="connectedAccount">
+      <div className="accountBlock">
+        <div className="blockTop">
+          <div className="connectedWith">Connected with MetaMask</div>
+          <div className="connectedActions">
             <Button>Disconnect</Button>
             <Button>Change</Button>
           </div>
         </div>
-        <div className='walletAddress'>
-          <div className='walletPic'></div>
-          <div className='walletID'>{props.sortAddress}</div>
+        <div className="walletAddress">
+          <div className="walletPic">
+            <i></i>
+          </div>
+          <div className="walletID">{props.sortAddress}</div>
         </div>
-        <div className='walletActions'>
-          <Button>Copy Address</Button>
+        <div className="walletActions">
+          <Button>
+            <i>
+              <CircleCheckIc />
+            </i>
+            <span>Copy Address</span>
+          </Button>
+          {/* <CC content={textToCopy} /> */}
           <Link
             href={"https://polygonscan.com/address/" + props.currentAccount}
             passHref
           >
             <a
-              target='_blank'
-              rel='noopener noreferrer'
+              target="_blank"
+              rel="noopener noreferrer"
               // className='button green rounded'
             >
+              <i>
+                <LinkExplorerIc size={18} />
+              </i>
               View on Explorer
             </a>
           </Link>
