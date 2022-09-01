6761b42601e4d85e45b46e5745ca907986c6739b
Vijay Bhatiya <vijaybhatiya007@gmail.com>
2022-09-01 12:02:36 +0530

    Fixed: My Contract page filters visible to desktop screen

--- components/my-contracts/FilterBar.js
+++ components/my-contracts/FilterBar.js
@@ -35,81 +35,81 @@ function FilterBar(props) {
         </span>
       </h2>
 
-      {isFiltersOpen === true && (
-        <div className="filtersBody">
-          <div className="filterCloseIc">
-            <CloseIc size="20" onClick={toggleFiltersFn} />
-          </div>
-          {/* Filter with Price */}
-          <div className="filterOption">
-            <h4 className="filterTitle">Price</h4>
-            <RadioGroup
-              selectedRadio={selectCurrency}
-              setSelectedRadio={setSelectCurrency}
-              values={currencyOptionsValues}
-            />
-            <MultiRangeSlider
-              min={0}
-              max={10}
-              onChange={
-                ({ min, max }) => {
-                  setFilterMaxPrice(`${max}`), setFilterMinPrice(`${min}`);
-                }
-                // console.log(`min = ${min}, max = ${max}`)
-              }
-            />
-            {/* params.priceFilterMinValue */}
-          </div>
-
-          {/* Filter with Wallet Address */}
-          <div className="filterOption">
-            <h4 className="filterTitle">Wallet Address</h4>
-            <input
-              className="formInput"
-              id="filterWalletAddress"
-              placeholder="Wallet Address"
-              type="text"
-              onBlur={walletAddressFn}
-            />
-          </div>
-
-          {/* Filter with Side */}
-          {params.filterSideAvailability !== false && (
-            <div className="filterOption">
-              <h4 className="filterTitle">Side</h4>
-              <RadioGroup
-                // listItem="radioList"
-                selectedRadio={filterSide}
-                setSelectedRadio={setFilterSide}
-                values={filterSideValues}
-              />
-            </div>
-          )}
-
-          {/* Filter with States */}
-          <div className="filterOption">
-            <h4 className="filterTitle">States</h4>
-
-            <SelectDropdown
-              selectedOption={filterStates}
-              setSelectedOption={setFilterStates}
-              options={dropDownOptions}
-            />
-          </div>
-
-          {/* Filter with Time to Deliver */}
-          {params.filterDurationAvailability !== false && (
-            <div className="filterOption">
-              <h4 className="filterTitle">Contract Duration</h4>
-              <RadioGroup
-                selectedRadio={filterDelivery}
-                setSelectedRadio={setFilterDelivery}
-                values={deliveryValues}
-              />
-            </div>
-          )}
+      <div
+        className={isFiltersOpen ? "filtersBody show" : "filtersBody"}
+      >
+        <div className="filterCloseIc">
+          <CloseIc size="20" onClick={toggleFiltersFn} />
         </div>
-      )}
+        {/* Filter with Price */}
+        <div className="filterOption">
+          <h4 className="filterTitle">Price</h4>
+          <RadioGroup
+            selectedRadio={selectCurrency}
+            setSelectedRadio={setSelectCurrency}
+            values={currencyOptionsValues}
+          />
+          <MultiRangeSlider
+            min={0}
+            max={10}
+            onChange={
+              ({ min, max }) => {
+                setFilterMaxPrice(`${max}`), setFilterMinPrice(`${min}`);
+              }
+              // console.log(`min = ${min}, max = ${max}`)
+            }
+          />
+          {/* params.priceFilterMinValue */}
+        </div>
+
+        {/* Filter with Wallet Address */}
+        <div className="filterOption">
+          <h4 className="filterTitle">Wallet Address</h4>
+          <input
+            className="formInput"
+            id="filterWalletAddress"
+            placeholder="Wallet Address"
+            type="text"
+            onBlur={walletAddressFn}
+          />
+        </div>
+
+        {/* Filter with Side */}
+        {params.filterSideAvailability !== false && (
+          <div className="filterOption">
+            <h4 className="filterTitle">Side</h4>
+            <RadioGroup
+              // listItem="radioList"
+              selectedRadio={filterSide}
+              setSelectedRadio={setFilterSide}
+              values={filterSideValues}
+            />
+          </div>
+        )}
+
+        {/* Filter with States */}
+        <div className="filterOption">
+          <h4 className="filterTitle">States</h4>
+
+          <SelectDropdown
+            selectedOption={filterStates}
+            setSelectedOption={setFilterStates}
+            options={dropDownOptions}
+          />
+        </div>
+
+        {/* Filter with Time to Deliver */}
+        {params.filterDurationAvailability !== false && (
+          <div className="filterOption">
+            <h4 className="filterTitle">Contract Duration</h4>
+            <RadioGroup
+              selectedRadio={filterDelivery}
+              setSelectedRadio={setFilterDelivery}
+              values={deliveryValues}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
